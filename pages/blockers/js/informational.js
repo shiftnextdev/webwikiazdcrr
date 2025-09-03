a063ad83bc54552ae7825a38772e255bb21739b4
nonoon <nonoon@SEA-1802233833.ant.amazon.com>
2025-09-03 17:50:45 -0400

    added icons to categories

--- pages/blockers/js/informational.js
+++ pages/blockers/js/informational.js
@@ -9,27 +9,29 @@ function replaceContainerContent(id, content) {
   container.innerHTML = content;
 }
 
-function moveSidebarScroll2Element(element){
+function moveSidebarScroll2Element(element) {
   const topval = element.getBoundingClientRect();
   const elMiddle = element.getBoundingClientRect().height / 2;
-  document.getElementById('heading-pointer').style.top = (topval.top-elMiddle)+"px";
+  document.getElementById("heading-pointer").style.top =
+    topval.top - elMiddle + "px";
   // console.log(topval.top-elMiddle);
 }
 
-const observer = new IntersectionObserver((entries)=>{
-  entries.forEach((entry)=>{
-    const linkElement = document.getElementById(entry.target.firstElementChild.id + "-link")
-    if(entry.isIntersecting){
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    const linkElement = document.getElementById(
+      entry.target.firstElementChild.id + "-link"
+    );
+    if (entry.isIntersecting) {
       // console.log(entry.target)
-      entry.target.classList.add("show")
-      moveSidebarScroll2Element(linkElement)
-      linkElement.classList.add("show")
-    }else{
-      entry.target.classList.remove("show")
-      linkElement.classList.remove("show")
+      entry.target.classList.add("show");
+      moveSidebarScroll2Element(linkElement);
+      linkElement.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+      linkElement.classList.remove("show");
     }
-    
-  })
-}, {})
-const contentBlocks = document.querySelectorAll(".content-block")
-contentBlocks.forEach(el => observer.observe(el))
+  });
+}, {});
+const contentBlocks = document.querySelectorAll(".content-block");
+contentBlocks.forEach((el) => observer.observe(el));
